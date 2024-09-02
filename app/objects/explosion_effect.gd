d63e66b212527102aa395f2400010519779a70e5
jacobabarbary <jacob@ferney.org>
2024-09-01 20:15:51 -0600

    added scene

--- app/objects/explosion_effect.gd
+++ app/objects/explosion_effect.gd
@@ -8,7 +8,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	get
 
 
 func _on_animated_sprite_2d_animation_finished():
